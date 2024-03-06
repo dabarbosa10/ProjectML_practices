e2d2a7eb0c22ee912cf46059d24013474f8837cc
NJBAza <njbuitragoa@gmail.com>
2024-03-06 12:54:48 -0500

    adding descriptions

--- RegLin.py
+++ RegLin.py
@@ -1,19 +1,34 @@
 import numpy as np
-
+import pandas as pd
 
 class Linreg:
 
-    def __init__(self, n_iter=1000, eta=0.05):
-        """
+    def __init__(self, n_iter: int = 1000, eta: float =0.05) -> object:
+        """_summary_
 
-        :type n_iter: object
-        """
+        Args:
+            n_iter (int, optional): _description_. Defaults to 1000.
+            eta (float, optional): _description_. Defaults to 0.05.
+
+        Returns:
+            object: _description_
+        """        
+
         self.theta = None
         self.cost = None
         self.eta = eta
         self.n_iter = n_iter
 
-    def fit(self, x, y):
+    def fit(self, x: pd.DataFrame, y: list[float])-> object:
+        """_summary_
+
+        Args:
+            x (pd.DataFrame): _description_
+            y (list[float]): _description_
+
+        Returns:
+            object: _description_
+        """        
         self.cost = []
         m = x.shape[0]
         self.theta = np.zeros(x.shape[1])
@@ -27,6 +42,14 @@ class Linreg:
         return self
 
     def predict(self, x):
+        """_summary_
+
+        Args:
+            x (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """        
         return np.dot(x. self.theta)
 
 
